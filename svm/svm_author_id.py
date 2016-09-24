4d8a16632ab26ba9e10422cd16d66194a535f8c6
Oji Ofong <ojiofong@gmail.com>
2016-09-23 23:55:05 -0400

    Used accuracy_score for SVM prediction

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,44 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+# clf = svm.LinearSVC()
+clf = svm.SVC(kernel='linear')
+
+# start timing
+t0 = time()
+
+# Slice data down to 1%
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
+
+clf.fit(features_train, labels_train)
+
+print("training/fitting time:", round(time()-t0, 3), "s")
+
+# start timing
+t0 = time()
+# prediction
+pred = clf.predict(features_test)
+
+print("prediction time:", round(time()-t0, 3), "s")
+
+# accuracy
+accuracy = clf.score(features_train, labels_train)
+print(accuracy)
+
+# Preferred accuracy API.. Uses pred
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print("Accuracy_score: ", acc)
+
+
+
+# SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+#     decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+#     max_iter=-1, probability=False, random_state=None, shrinking=True,
+#     tol=0.001, verbose=False)
+
 #########################################################
 
 
