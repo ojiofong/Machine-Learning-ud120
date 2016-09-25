9d6e5f82aa8110f26a930275f39a94e0175dda4a
Oji Ofong <ojiofong@gmail.com>
2016-09-25 11:01:15 -0400

    Completed Decision Tree mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+print("\nSTART - Decision tree mini project\n")
+
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print("Accuracy_score: ", acc)
+print("Num of features: ", len(features_train[0]))
+
+print("\nEND - Decision tree mini project\n")
 
 #########################################################
 
