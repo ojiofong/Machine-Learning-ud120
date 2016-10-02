83ed921ef30c386425179c5c16fba6beb2066598
Oji Ofong <ojiofong@gmail.com>
2016-10-01 23:21:46 -0400

    Completed exploring the Enron dataset mini project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,40 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+
+total = len(enron_data)
+print("enron data total:", total)
+# print(enron_data)
+# print(enron_data.keys())
+# print(enron_data.values())
+
+countPOI = 0;
+countSalary = 0;
+countEmail = 0;
+countTotalPaymentNaN = 0;
+
+for key,value in enron_data.items():
+    # print(key, 'corresponds to', value)
+    if enron_data[key]["poi"]:
+            countPOI += 1
+
+    if enron_data[key]["salary"] != 'NaN':
+        countSalary += 1
+
+    if enron_data[key]["email_address"] != 'NaN':
+        countEmail += 1
+
+    if enron_data[key]["poi"] and enron_data[key]["total_payments"] == 'NaN':
+        countTotalPaymentNaN += 1
+
+print("Total POI count:", countPOI)
+print("Total Quantified Salary count:", countSalary)
+print("Total Known Email:", countEmail)
+print("Total Payments NaN:", countTotalPaymentNaN/countPOI * 100, "%")
+
+print("\nJames Prentice's total stocks:", enron_data["PRENTICE JAMES"]['total_stock_value'])
+print("email from Wesley Colwell to POI:", enron_data["COLWELL WESLEY"]['from_this_person_to_poi'])
+print("Jeffrey K Skilling's stock options:", enron_data["SKILLING JEFFREY K"]['exercised_stock_options'])
 
