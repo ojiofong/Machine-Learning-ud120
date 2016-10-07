34afd7daf93a03d90a5d1f3da642032d1642c371
Oji Ofong <ojiofong@gmail.com>
2016-10-07 02:49:58 -0400

    Completed outliers mini project

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -3,17 +3,42 @@
 import pickle
 import sys
 import matplotlib.pyplot
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+
+# Found key "TOTAL" as outlier
+# Removing the outlier before generating the data
+data_dict.pop('TOTAL', 0)
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
-
 ### your code below
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter(salary, bonus)
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
+# How to find outlier
+# Found key "TOTAL" as outlier
+mMax = 0
+mKey = ''
+for key, value in data_dict.items():
+    bonus = value['bonus']
+    if bonus != 'NaN' and bonus > mMax:
+        mMax = bonus
+        mKey = key
+        print("key", mKey)
+
+print("final key", mKey)
+print("final bonus", mMax)
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,19 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
 
-    
-    return cleaned_data
+    error = (predictions - net_worths) ** 2  # create list of error
+    cleaned_data = zip(ages, net_worths, error)  # Merge tuple
+    cleaned_data = sorted(cleaned_data, key=lambda tup: tup[2])  # sort by tuple index 2 i.e. error
+    limit = len(cleaned_data) * 0.9  # 90% of length
+    cleaned_data = cleaned_data[:int(limit - 1)]  # Remove last 10% or retain first 90%
 
+    # print("limit ", limit - 1)
+    # print("length ", len(cleaned_data))
+    # print("cleaned_data ", cleaned_data)
+
+    return cleaned_data
